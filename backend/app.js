9816f68c6d8c326f3f5d68e89102e67dd236c883
starr <starr@DESKTOP-P30J3OU>
2023-11-01 13:11:58 +0100

    first commit

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,9 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors({
-  origin: ['https://eshop-tutorial-pyri.vercel.app',],
-  credentials: true
-}));
-
+app.use(cors());
+// origin: ['localhost/3000,],
+// credentials: true
 app.use(express.json());
 app.use(cookieParser());
 app.use("/test", (req, res) => {
@@ -26,27 +24,29 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 // import routes
-const user = require("./controller/user");
-const shop = require("./controller/shop");
-const product = require("./controller/product");
-const event = require("./controller/event");
-const coupon = require("./controller/coupounCode");
-const payment = require("./controller/payment");
-const order = require("./controller/order");
-const conversation = require("./controller/conversation");
-const message = require("./controller/message");
-const withdraw = require("./controller/withdraw");
+// const user = require("./controller/user");
+// const shop = require("./controller/shop");
+// const product = require("./controller/product");
+// const event = require("./controller/event");
+// const coupon = require("./controller/coupounCode");
+// const payment = require("./controller/payment");
+// const order = require("./controller/order");
+// const conversation = require("./controller/conversation");
+// const message = require("./controller/message");
+// const withdraw = require("./controller/withdraw");
 
-app.use("/api/v2/user", user);
-app.use("/api/v2/conversation", conversation);
-app.use("/api/v2/message", message);
-app.use("/api/v2/order", order);
-app.use("/api/v2/shop", shop);
-app.use("/api/v2/product", product);
-app.use("/api/v2/event", event);
-app.use("/api/v2/coupon", coupon);
-app.use("/api/v2/payment", payment);
-app.use("/api/v2/withdraw", withdraw);
+
+// use routes
+// app.use("/api/v2/user", user);
+// app.use("/api/v2/conversation", conversation);
+// app.use("/api/v2/message", message);
+// app.use("/api/v2/order", order);
+// app.use("/api/v2/shop", shop);
+// app.use("/api/v2/product", product);
+// app.use("/api/v2/event", event);
+// app.use("/api/v2/coupon", coupon);
+// app.use("/api/v2/payment", payment);
+// app.use("/api/v2/withdraw", withdraw);
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
